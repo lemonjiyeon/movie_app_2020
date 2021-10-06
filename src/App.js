1c864f5e5bac81954ecf087d1cc813a235ff8875
lemonjiyeon <govl2411@naver.com>
2021-10-06 12:04:25 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from "./components/Navigation";
+import Detail from './routes/Detail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App(){
+  return ( 
+  <HashRouter>
+    <Navigation/>
+    <Route path="/" exact={true} component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/movie-detail" component={Detail}/>
+  </HashRouter>
   );
 }
+
+// class App extends React.Component {
+
+//   state = {
+//     isLoading: true,
+//     movies: [],
+//   };
+
+//   getMovies = async () => {
+//     const{
+//       data: {
+//         data: {movies},
+//       },
+//     }  = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+//     console.log(movies);
+//     this.setState({movies, isLoading: false});
+
+//   }
+
+//   componentDidMount(){
+//     this.getMovies();
+//   }
+  
+//   render(){
+//     const {isLoading,movies} = this.state;
+//     return (
+// <section className="container">
+//       {isLoading ?
+//       ( 
+//         <div className="loader">
+//          <span className="loader__text">Loading...</span>
+//         </div>
+//          ): (
+//       <div className="movies">
+//         {movies.map(movie => (
+//             <Movie
+//               key={movie.id}
+//               id={movie.id}
+//               year={movie.year}
+//               title={movie.title}
+//               summary={movie.summary}
+//               poster={movie.medium_cover_image}
+//               genres={movie.genres}
+//             />
+//         ))}
+//       </div>
+//       )}
+// </section>
+//     );
+//   }
+// }
 
 export default App;
